9fda4b5f36a0f7a74a3877c706f21a573cecef16
JONATHAS GABRIEL FERREIRA DE SOUZA SANTOS <232.932094@universidadesenaicimatec.edu.br>
2025-05-08 10:52:41 -0300

    Posicionando Mensagem de boas vindas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, HomeComponent],
+  imports: [RouterOutlet, LoginComponent, HomeComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
